82dc8983cb8c51b3bea31658fcd76adc067e4503
Alexander Frankel <alexxander.frankel@gmail.com>
2017-11-13 17:50:06 -0500

    Create users controller

---
+++ spec/controllers/users_controller_spec.rb
@@ -0,0 +1,23 @@
+require 'rails_helper'
+
+describe UsersController, type: :controller do
+
+  describe "POST #create" do
+    context "with valid attributes" do
+      it "creates a new user" do
+      end
+
+      it "returns a success" do
+      end
+    end
+
+    context "with invalid attributes" do
+      it "does not create a new user" do
+      end
+
+      it "returns an error" do
+      end
+    end
+  end
+
+end
